d6393255840d33c580edf60574754383ce3fe26d
ced <ced@c30.life>
2025-11-28 21:21:07 +0900

    fix '"puddingClicked"' is not assignable to type

--- packages/backend/src/models/UserProfile.ts
+++ packages/backend/src/models/UserProfile.ts
@@ -369,7 +369,7 @@ export const ACHIEVEMENT_TYPES = [
 	'clickedClickHere',
 	'justPlainLucky',
 	'setNameToSyuilo',
-	'cookieClicked',
+	'puddingClicked',
 	'brainDiver',
 	'smashTestNotificationButton',
 	'tutorialCompleted',
